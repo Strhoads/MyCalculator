9961d6a84eeaa9f57c5ef7d8c0117a1ffbe0f24a
Steven Rhoads <strhoads@gmail.com>
2017-09-14 14:04:21 -0400

    Calculator Drill

---
+++ MyCalculator/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MyCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .newStyle1 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-family: Arial, Helvetica, sans-serif">
+            <h1 style="font-family: Arial; font-weight: 900">Simple Calulator</h1>
+            <br />
+            <span class="newStyle1">First Value:&nbsp;</span>
+            <asp:TextBox ID="firstTextBox" runat="server"></asp:TextBox>
+            <br />
+            Second Value:&nbsp;
+            <asp:TextBox ID="secondTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="addButton" runat="server" Text="+" OnClick="addButton_Click" />
+&nbsp;
+            <asp:Button ID="subButton" runat="server" Text="-" OnClick="subButton_Click" />
+&nbsp;
+            <asp:Button ID="multButton" runat="server" Text="*" OnClick="multButton_Click" />
+&nbsp;
+            <asp:Button ID="divButton" runat="server" Text="/" OnClick="divButton_Click" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server" Font-Bold="True"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ MyCalculator/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            int first= int.Parse(firstTextBox.Text);
+            int second = int.Parse(secondTextBox.Text);
+
+            int result = first + second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void subButton_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(firstTextBox.Text);
+            int second = int.Parse(secondTextBox.Text);
+
+            int result = first - second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void multButton_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(firstTextBox.Text);
+            int second = int.Parse(secondTextBox.Text);
+
+            int result = first * second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void divButton_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(firstTextBox.Text);
+            int second = int.Parse(secondTextBox.Text);
+
+            int result = first / second;
+            resultLabel.Text = result.ToString();
+        }
+    }
+}
